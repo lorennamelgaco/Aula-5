e542c769e18cc0b4efb4dece927765359ab762ee
lorennamelgaco <lorenna.melgaco@uni9.edu.br>
2026-06-07 22:27:04 +0000

    Aula 6

---
+++ validacao.js
@@ -0,0 +1,30 @@
+const formulario = document.querySelector("form");
+
+formulario.addEventListener("submit", function(event) {
+
+  const nome  = document.getElementById("nome").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const idade = Number(document.getElementById("idade").value);
+
+  const erroEl = document.getElementById("erro");
+  erroEl.textContent = "";
+
+  if (nome === "") {
+    event.preventDefault();
+    erroEl.textContent = "⚠️ O campo Nome não pode estar vazio.";
+    return;
+  }
+
+  if (!email.includes("@")) {
+    event.preventDefault();
+    erroEl.textContent = "⚠️ O e-mail precisa conter @.";
+    return;
+  }
+
+  if (isNaN(idade) || idade <= 0) {
+    event.preventDefault();
+    erroEl.textContent = "⚠️ A idade deve ser maior que 0.";
+    return;
+  }
+
+});
